47cba8f63cfc3ae7ee3c6c94161fb5be3399a80a
Martin Pugh <pugh@s3kr.it>
2018-04-14 15:55:10 +0200

    Adjust parameters and set door status based on pin, on boot

--- src/main.ino
+++ src/main.ino
@@ -8,22 +8,22 @@
 // Motor speed
 #define SPEED 128
 // Time to sleep after moving door (seconds)
-#define SLEEPTIME 1000
+#define SLEEPTIME 600
 // Threshold before it is considered 'morning'
-#define LIGHT_THRESHOLD 250
+#define LIGHT_THRESHOLD 100
 // Threshold before it is considered 'night'
-#define DARK_THRESHOLD 250
+#define DARK_THRESHOLD 100
 
 // Pins
-#define ROOF_PIN 2
-#define FLOOR_PIN 3
+#define ROOF_PIN 3
+#define FLOOR_PIN 2
 #define ENABLE_PIN 11
 #define MOTOR_PIN_1 10
 #define MOTOR_PIN_2 9
 #define PHOTORESISTOR_PIN A0
 
 int sensorValue = 0;
-boolean doorStatus = DOWN;
+boolean doorStatus;
 
 void setup()
 {
@@ -35,35 +35,33 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(ROOF_PIN, HIGH);
   digitalWrite(FLOOR_PIN, HIGH);
-  Serial.begin(9600);
-  Serial.println("Initiated");
+
+  // Set door status depending on which pin is pressed
+  doorStatus = digitalRead(ROOF_PIN);
 }
 
 void loop()
 {
   // Sleep for 1 minute
-  lowPowerSleep(60);
+  delay(500);
 
-  sensorValue = analogRead(PHOTORESISTOR_PIN);
-
-  Serial.print("sensor = ");
-  Serial.println(sensorValue);
+  sensorValue = analogRead(A0);
 
   //if it is daylight, & the last action is NOT opening, open the door
   if (sensorValue >= LIGHT_THRESHOLD && doorStatus == DOWN){
-    Serial.println("Opening door");
     moveDoor(UP);
     doorStatus = UP;
     // Now we sleep for some time to prevent 'flickering'
+    delay(500);
     lowPowerSleep(SLEEPTIME);
   }
 
   //if it is night, & the last action is NOT closing, close the door
   if (sensorValue <= DARK_THRESHOLD && doorStatus == UP){
-    Serial.println("Closing door");
     moveDoor(DOWN);
     doorStatus = DOWN;
     // Now we sleep for some time to prevent 'flickering'
+    delay(500);
     lowPowerSleep(SLEEPTIME);
   }
 }
@@ -78,18 +76,12 @@ void moveDoor(boolean direction){
   if (direction == UP)
     pin = ROOF_PIN;
 
-  Serial.print(digitalRead(FLOOR_PIN));
-  Serial.print(" ");
-  Serial.println(digitalRead(ROOF_PIN));
-
   analogWrite(ENABLE_PIN, SPEED);
   digitalWrite(MOTOR_PIN_1, !direction);
   digitalWrite(MOTOR_PIN_2, direction);
-  Serial.println(digitalRead(pin));
   while(!digitalRead(pin)){
     (void)0;
   }
-  Serial.println("Pin hit! Stopping motors");
   analogWrite(ENABLE_PIN, 0);
 }
 
